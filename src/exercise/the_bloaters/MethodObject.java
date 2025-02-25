71c25eb3314626e742733576f8752c2a363b2e5e
Galih-clkwrk <galihilham66@gmail.com>
2025-02-25 20:09:23 +0700

    Update MethodObject.java

--- src/exercise/the_bloaters/MethodObject.java
+++ src/exercise/the_bloaters/MethodObject.java
@@ -1,32 +1,65 @@
 package exercise.the_bloaters;
-
 public class MethodObject {
-
 	class Account {
 		private int delta() {
 			return 10;
 		}
-		// TODO: replace this gamma method with method object (make a class for the gamma logic)  
+		
 		public int gamma(int inputVal, int quantity, int yearToDate) {
-			    int importantValue1 = (inputVal * quantity) + delta();
-			    int importantValue2 = (inputVal * yearToDate) + 100;
-			    if ((yearToDate - importantValue1) > 100) {
-			      importantValue2 -= 20;
-			    }
-			    int importantValue3 = importantValue2 * 7;
-			    // and so on�
-			    return importantValue3 - 2 * importantValue1;
-			  }
-
+			return new GammaCalculator(this, inputVal, quantity, yearToDate).compute();
+		}
 	}
+	
+	class GammaCalculator {
+		private final Account account;
+		private final int inputVal;
+		private final int quantity;
+		private final int yearToDate;
+		private int importantValue1;
+		private int importantValue2;
+		private int importantValue3;
+		
+		public GammaCalculator(Account account, int inputVal, int quantity, int yearToDate) {
+			this.account = account;
+			this.inputVal = inputVal;
+			this.quantity = quantity;
+			this.yearToDate = yearToDate;
+		}
+		
+		public int compute() {
+			calculateImportantValue1();
+			calculateImportantValue2();
+			calculateImportantValue3();
+			return finalCalculation();
+		}
+		
+		private void calculateImportantValue1() {
+			importantValue1 = (inputVal * quantity) + account.delta();
+		}
+		
+		private void calculateImportantValue2() {
+			importantValue2 = (inputVal * yearToDate) + 100;
+			if ((yearToDate - importantValue1) > 100) {
+				importantValue2 -= 20;
+			}
+		}
+		
+		private void calculateImportantValue3() {
+			importantValue3 = importantValue2 * 7;
+		}
+		
+		private int finalCalculation() {
+			return importantValue3 - 2 * importantValue1;
+		}
+	}
+	
 	private void test() {
 		Account account = new Account();
 		System.out.println(account.gamma(15,10,2010));
-		
 	}
+	
 	public static void main(String[] args) {
 		MethodObject p = new MethodObject();
 		p.test();
 	}
-
 }
