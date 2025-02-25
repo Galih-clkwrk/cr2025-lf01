b1dfd6dab18062e2da64f3c2dca71f23e3ba085e
Galih-clkwrk <galihilham66@gmail.com>
2025-02-25 19:59:13 +0700

    Update ExtractClass.java

--- src/exercise/the_bloaters/ExtractClass.java
+++ src/exercise/the_bloaters/ExtractClass.java
@@ -1,28 +1,61 @@
 package exercise.the_bloaters;
 public class ExtractClass {
 // todo extract class TelephoneNumber from class Person
-	class Person {
-	  private String name;
-	  private String officeAreaCode;
-	  private String officeNumber;
-
-	  public String getName() {
-		return name;
-	  }
-	  public String getTelephoneNumber() {
-		return ("(" + officeAreaCode + ") " + officeNumber);
-	  }
-	  public String getOfficeAreaCode() {
-		return officeAreaCode;
-	  }
-	  public void setOfficeAreaCode(String arg) {
-		officeAreaCode = arg;
-	  }
-	  public String getOfficeNumber() {
-		return officeNumber;
-	  }
-	  public void setOfficeNumber(String arg) {
-		officeNumber = arg;
-	  }
-	}
+    class Person {
+        private String name;
+        private TelephoneNumber telephoneNumber;
+        
+        public Person() {
+            this.telephoneNumber = new TelephoneNumber();
+        }
+        
+        public String getName() {
+            return name;
+        }
+        
+        public String getTelephoneNumber() {
+            return telephoneNumber.getTelephoneNumber();
+        }
+        
+        public String getOfficeAreaCode() {
+            return telephoneNumber.getAreaCode();
+        }
+        
+        public void setOfficeAreaCode(String arg) {
+            telephoneNumber.setAreaCode(arg);
+        }
+        
+        public String getOfficeNumber() {
+            return telephoneNumber.getNumber();
+        }
+        
+        public void setOfficeNumber(String arg) {
+            telephoneNumber.setNumber(arg);
+        }
+    }
+    
+    class TelephoneNumber{
+        private String areaCode;
+        private String number;
+        
+        public String getTelephoneNumber(){
+            return ("(" + areaCode + ") " + number);
+        }
+        
+        public String getAreaCode(){
+            return areaCode;
+        }
+        
+        public void setAreaCode(String arg){
+            areaCode = arg;
+        }
+        
+        public String getNumber(){
+            return number;
+        }
+        
+        public void setNumber(String arg){
+            number = arg;
+        }
+    }
 }
